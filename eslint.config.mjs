67ed94a0ea3d3e47f37207adfb77f55a382bb53a
Carl Sverre <82591+carlsverre@users.noreply.github.com>
2025-12-29 15:38:38 -0800

    update eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+// eslint.config.mjs
+import tsparser from "@typescript-eslint/parser";
+import { defineConfig, globalIgnores } from "eslint/config";
+import obsidianmd from "eslint-plugin-obsidianmd";
+
+export default defineConfig([
+  ...obsidianmd.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: { project: "./tsconfig.json" },
+    },
+    rules: {},
+  },
+  globalIgnores(["node_modules/*", "main.js"]),
+]);
